4b13733f023a9c73927d61d9a307d4c269fe22ff
Umang Patel <69800542+UmangX@users.noreply.github.com>
2021-11-21 15:30:07 +0530

    Update umangxbot.py

--- Code/umangxbot.py
+++ Code/umangxbot.py
@@ -4,16 +4,10 @@ from telegram import *
 from telegram.ext import *
 from random import randrange
 
-
 def getusername(update, context):
     print(update.message.from_user['username'])
-
-
-
 Startup_time = time.asctime((time.localtime(time.time())))
-
 bot = Bot("Your API Key")
-
 pictures = ("https://photos.app.goo.gl/49xpGxEKVAQBGEA7A","https://photos.app.goo.gl/EQRdTZAWk71F5UXU9","https://photos.app.goo.gl/CGDT9yojjRzot6Hb8","https://photos.app.goo.gl/c1U4ofczKu6Ag3NVA","https://photos.app.goo.gl/UjzBFUGiDRA9wb9c8")
 
 def quotess(update:Update,context:CallbackContext):
@@ -36,7 +30,6 @@ def pic(update:Update,context:CallbackContext):
     bot.send_photo(chat_id = update.effective_chat.id,photo = pictures[randrange(0,4)])
     print("Command Executed") 
     
-
 def whatcanudo(update:Update,context:CallbackContext):
     bot.send_message(
         chat_id = update.effective_chat.id,
@@ -44,16 +37,12 @@ def whatcanudo(update:Update,context:CallbackContext):
     )
     print("Command Executed") 
 
-
-
-
 def startup(update:Update,context:CallbackContext):
     bot.send_message(
         chat_id = update.effective_chat.id,
         text="I am the shadow of umang (May be) and this is a Telegram bot type /whatcanudo for Commands"
     )
     print("Command Executed") 
-
 
 def randomimages(update:Update,context:CallbackContext):
     newdata = requests.get("https://yesno.wtf/api")
@@ -66,18 +55,12 @@ def randomimages(update:Update,context:CallbackContext):
     )
     print("Command Executed") 
 
-
-
 def runsince(update:Update,context:CallbackContext):
     bot.send_message(
         chat_id = update.effective_chat.id,
         text=Startup_time
-
     )
     print("Command Executed") 
-
-
-
 
 def main():    
     bot = Bot("Your API Key")
@@ -99,12 +82,9 @@ def main():
     dispatcher.add_handler(hellomsg)
     updater.start_polling()
 
-
-
 try:    
     print("Bot has Started")
     main()
-
 except KeyboardInterrupt:
     print("Bot Is Going To Sleep")     
-    
+  
